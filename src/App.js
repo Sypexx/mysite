d79adf769232d199794d64dc6023e7e03d72f518
Sypexx <Sypexofficial@gmail.com>
2025-04-07 23:27:58 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import SocialLinks from "./SocialLinks";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SocialLinks />
+      </header>
+    </div>
+  );
+}
+
+export default App;
