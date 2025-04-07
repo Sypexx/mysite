2f0f5b5bb2825961a4455fdc2f79110eb731b03e
Sypexx <Sypexofficial@gmail.com>
2025-04-07 23:58:55 +0900

    abcd1234

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import "./App.css";
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import SocialLinks from "./SocialLinks";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <SocialLinks />
-      </header>
-    </div>
+    <Router basename="/mysite">
+      <div className="App">
+        <header className="App-header">
+          <SocialLinks />
+        </header>
+      </div>
+    </Router>
   );
 }
 
